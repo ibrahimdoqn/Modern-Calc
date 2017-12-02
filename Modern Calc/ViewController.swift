23d43918c2ba13174595cf31f733068360115e53
İbrahim DOĞAN <33070834+ibrahimdoqn@users.noreply.github.com>
2017-12-02 22:28:21 +0300

    Initial Commit

---
+++ Modern Calc/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Modern Calc
+//
+//  Created by Wantto on 2.12.2017.
+//  Copyright © 2017 Wantto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
